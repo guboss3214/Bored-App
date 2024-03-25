3aafc7d1b54a92c61c3654e2c1837821325f8e5f
guboss3214 <uraporskalo@gmail.com>
2024-03-25 20:45:27 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+const button = document.querySelector('#button');
+const text_block = document.querySelector('.bored_text');
+const link_text = document.querySelector('.link_text');
+
+
+async function bored(){
+    const response = await fetch('http://www.boredapi.com/api/activity/');
+
+    const data = response.json();
+
+   return data;
+}
+
+async function handleActivity(){
+    const activity = await bored();
+
+    try{
+        text_block.innerHTML = activity.activity;
+        link_text.innerHTML = activity.link;
+        link_text.href = activity.link;
+    }
+    catch{
+        console.log(error);
+    }
+}
+
+button.addEventListener('click', () => {
+    handleActivity();
+});
+
+console.log(link_text.href);
